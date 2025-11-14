433fba4a71b9079bb238ebdefc54a7ef8f5f9802
Nikhil170224 <nikhilmi1050@gmail.com>
2025-11-15 01:03:45 +0530

    exploring the GET, POST, DELETE, HTTP methods

--- src/app.js
+++ src/app.js
@@ -2,13 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.use("/note", (req, res) => {
-    res.send("hello your server");
+app.use("/user", (req, res) => {
+  res.send("haaahahahha");
+});
+
+app.get("/user", (req, res) => {
+    res.send({name: "Nikhil", rollno: "150"});
 })
 
-app.use("/about", (req, res) => {
+app.post("/user", (req, res) => {
+    console.log("Data has been stored to database");
+    res.send("data has been stored to database");
+});
+
+app.delete("/user", (req,res) => {
+    res.send("Data deleted succesfully");
+});
+app.use("/hello/about", (req, res) => {
     res.send("hello this is the about section")
 })
+app.use("/", (req, res) => {
+  res.send("hello your server");
+});
 app.listen(7777, () => {
     console.log("hello you are successfully using port 7777....");
 })
