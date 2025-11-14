b791b203e1d7f5d1c1f30d406bec54269456cd85
Nikhil170224 <nikhilmi1050@gmail.com>
2025-11-15 01:52:22 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,28 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res) => {
-  res.send("haaahahahha");
-});
-
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name", (req, res) => {
+    console.log(req.params);
     res.send({name: "Nikhil", rollno: "150"});
 })
 
-app.post("/user", (req, res) => {
-    console.log("Data has been stored to database");
-    res.send("data has been stored to database");
-});
-
-app.delete("/user", (req,res) => {
-    res.send("Data deleted succesfully");
-});
-app.use("/hello/about", (req, res) => {
-    res.send("hello this is the about section")
-})
-app.use("/", (req, res) => {
-  res.send("hello your server");
-});
 app.listen(7777, () => {
     console.log("hello you are successfully using port 7777....");
 })
